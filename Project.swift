93ad509cb1a684c269d125955e875413b4c4842e
petitstrawberry <petitstrawberry.dev@gmail.com>
2026-05-03 17:32:49 +0900

    feat: KanadeKitのバージョンを0.5.0から0.6.0に更新

--- Project.swift
+++ Project.swift
@@ -4,7 +4,7 @@ let project = Project(
     name: "KanadeApp",
     organizationName: "petitstrawberry",
     packages: [
-        .package(url: "https://github.com/petitstrawberry/KanadeKit.git", from: "0.5.0"),
+        .package(url: "https://github.com/petitstrawberry/KanadeKit.git", from: "0.6.0"),
         .package(url: "https://github.com/vtourraine/AcknowList.git", from: "3.0.0"),
     ],
     targets: [
